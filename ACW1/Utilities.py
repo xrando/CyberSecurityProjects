1cd1c33917ce3cfb4c58cb105161f7d2d77ae7a6
NeoTianHow <tianhow1234@hotmail.com>
2023-05-22 23:47:08 +0800

    Merged Conflicts

---
+++ ACW1/Utilities.py
@@ -0,0 +1,27 @@
+#pip install python-docx openpyxl
+import docx
+import openpyxl
+
+def read_file_content(file_path):
+    file_extension = file_path.split('.')[-1].lower()
+
+    if file_extension == 'txt':
+        with open(file_path, 'r') as file:
+            content = file.read()
+        return content
+    elif file_extension == 'docx':
+        doc = docx.Document(file_path)
+        content = '\n'.join([paragraph.text for paragraph in doc.paragraphs])
+        return content
+    elif file_extension == 'xlsx':
+        workbook = openpyxl.load_workbook(file_path)
+        content = []
+        for sheet_name in workbook.sheetnames:
+            sheet = workbook[sheet_name]
+            for row in sheet.iter_rows(values_only=True):
+                content.append('\t'.join(str(cell) for cell in row))
+        return '\n'.join(content)
+    else:
+        return None
+
+print(read_file_content("t.docx"))
--- ACW1/display.py
+++ ACW1/display.py
@@ -1,17 +1,29 @@
 from StegnoImg import imgSteg
-import cv2
-import tkinter  
+import cv2 
+from Utilities import read_file_content
+from tkinter import *
+from tkinter import ttk
 
 steg = imgSteg()
 
 if __name__ == "__main__":
-	input_image = "t.PNG"
-	output_image = "encoded_image.PNG"
-	secret_data = "test message"
+	input_image = "test.jpg"
+	#standardize output to be png to prevent loss of metadata due to jpg utilizing lossless compression
+	output_image = "encoded_image.png"
+	#secret_data = "test message"
+	secret_data_file = "t.docx"
 	# encode the data into the image
-	encoded_image = steg.encode(img=input_image, message=secret_data, bits = 1)
+	encoded_image = steg.encode(img=input_image, message=read_file_content(secret_data_file), bits = 1)
 	# save the output image (encoded image)
 	cv2.imwrite(output_image, encoded_image)
 	# decode the secret data from the image
 	decoded_data = steg.decode(output_image, bits = 1)
 	print("[+] Decoded data:", decoded_data)
+
+	root = Tk()
+	frm = ttk.Frame(root, width=400, height=300, padding=10)
+	frm.pack()
+	ttk.Label(frm, text="Hello World!").place(x=20, y=20)
+	
+	ttk.Button(frm, text="Quit", command=root.destroy).place(x=50, y=50)
+	root.mainloop()
