44be75f360c1b5b987dfaa03478bf1ca78c9f2d8
Benjamin Loh <xrando20@gmail.com>
2023-05-22 10:03:58 +0800

    temp fix to jpg encode/decode issue
    temp fix to jpg encode/decode issue

--- ACW1/display.py
+++ ACW1/display.py
@@ -5,10 +5,11 @@ from Utilities import read_file_content
 steg = imgSteg()
 
 if __name__ == "__main__":
-	input_image = "t.PNG"
-	output_image = "encoded_image.PNG"
+	input_image = "test.jpg"
+	#standardize output to be png to prevent loss of metadata due to jpg utilizing lossless compression
+	output_image = "encoded_image.png"
 	#secret_data = "test message"
-	secret_data_file = "t.txt"
+	secret_data_file = "t.docx"
 	# encode the data into the image
 	encoded_image = steg.encode(img=input_image, message=read_file_content(secret_data_file), bits = 1)
 	# save the output image (encoded image)
