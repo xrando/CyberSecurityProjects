3ffd132717cc09b1bc416f3b8ecbc8d35f160c3b
Benjamin Loh <xrando20@gmail.com>
2023-05-22 09:36:54 +0800

    test multi file input
    test multi file input

--- ACW1/display.py
+++ ACW1/display.py
@@ -1,15 +1,16 @@
 from StegnoImg import imgSteg
 import cv2
 import tkinter  
-
+from Utilities import read_file_content
 steg = imgSteg()
 
 if __name__ == "__main__":
 	input_image = "t.PNG"
 	output_image = "encoded_image.PNG"
-	secret_data = "test message"
+	#secret_data = "test message"
+	secret_data_file = "t.txt"
 	# encode the data into the image
-	encoded_image = steg.encode(img=input_image, message=secret_data, bits = 1)
+	encoded_image = steg.encode(img=input_image, message=read_file_content(secret_data_file), bits = 1)
 	# save the output image (encoded image)
 	cv2.imwrite(output_image, encoded_image)
 	# decode the secret data from the image
